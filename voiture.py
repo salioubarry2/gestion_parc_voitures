13d05a0009bf8bb8d714465fe1f27c795b71b53f
Saliou Barry <salioubarry@MacBookPro-de-Saliou.local>
2026-03-05 21:05:33 -0500

    Creation de la classe voiture

---
+++ voiture.py
@@ -0,0 +1,9 @@
+class Voiture:
+    def __init__(self, matricule ,marque, couleur):
+        self.matricule = matricule
+        self.marque = marque
+        self.couleur = couleur
+    def afficherinformation(self):
+        print("Matricule:",self.matricule)
+        print("Marque:",self.marque)
+        print("Couleur:",self.couleur)
