6afe77e6c036c2b3984112607e933da79aafac00
Saliou Barry <salioubarry@MacBookPro-de-Saliou.local>
2026-03-07 17:54:55 -0500

    la classe voiture et ses attributs sont ajouté

--- voiture.py
+++ voiture.py
@@ -3,7 +3,7 @@ class Voiture:
         self.matricule = matricule
         self.marque = marque
         self.couleur = couleur
-    def afficherinformation(self):
+    def afficher_information(self):
         print("Matricule:",self.matricule)
         print("Marque:",self.marque)
         print("Couleur:",self.couleur)
