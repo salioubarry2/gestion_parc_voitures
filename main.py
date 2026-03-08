84cd5b73638a9b353ac0dad706032f6a4f37e884
Saliou Barry <salioubarry@MacBookPro-de-Saliou.local>
2026-03-08 17:20:42 -0400

    creation du fichier main et initialisation du parc et voiture

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 from voiture import Voiture
 from parc import Parc
+
+parc1= Parc(1 , "Toronto", 3)
+
+voiture1 = Voiture("11", "NISAN","NOIR")
+voiture2 = Voiture("12", "TOYOTA", "GRIS")
+voiture3 = Voiture("13", "HUNDA", "BLANC")
+
+
+
+
+
