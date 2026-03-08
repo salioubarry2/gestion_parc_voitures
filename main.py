2af85d297381aef2ad6827935c8506f04ba825be
Saliou Barry <salioubarry@MacBookPro-de-Saliou.local>
2026-03-08 17:36:42 -0400

    Test des methodes entrer_voiture et sortir_voiture

--- main.py
+++ main.py
@@ -8,6 +8,20 @@ voiture2 = Voiture("12", "TOYOTA", "GRIS")
 voiture3 = Voiture("13", "HUNDA", "BLANC")
 
 
+voiture1.afficher_information()
+voiture2.afficher_information()
+voiture3.afficher_information()
 
 
+parc1.entrer_voiture(voiture1)
+parc1.entrer_voiture(voiture2)
+parc1.entrer_voiture(voiture3)
+
+
+print("Places disponibles est:",parc1.calculer_nbr_place_dispo())
+
+parc1.sortir_voiture(voiture2)
+
+
+print("Places disponible apres la sortie est:",parc1.calculer_nbr_place_dispo())
 
